dba812bdfa7a6f22c9d5a348fd8a9eeb0e8adf10
wolffewrought <wolffewrought@gmail.com>
2026-08-21 17:25:30 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * Bump CACHE_VERSION on every deploy to evict old entries.
  */
 
-var CACHE_VERSION = 'sat-v124';
+var CACHE_VERSION = 'sat-v132';
 var CACHE_NAME = 'security-access-tracker-' + CACHE_VERSION;
 
 var PRECACHE = [
